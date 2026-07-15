c627e458eb49e6aae38ac8443e4e277e0636b7d1
Teashrock <kajitsu22@gmail.com>
2026-07-15 21:01:16 +0300

    Flake8

--- app/create_file.py
+++ app/create_file.py
@@ -5,13 +5,13 @@ import sys
 
 dir_idx = -1
 file_idx = -1
-file = ""
+file_name = ""
 
 if "-d" in sys.argv:
     dir_idx = sys.argv.index("-d")
 if "-f" in sys.argv:
     file_idx = sys.argv.index("-f")
-    file = sys.argv[file_idx + 1]
+    file_name = sys.argv[file_idx + 1]
 
 dirs = []
 if dir_idx > -1:
@@ -25,7 +25,7 @@ if dir_idx > -1:
 
 if dirs:
     os.makedirs(os.path.join(*dirs))
-if file:
+if file_name:
     file_contents = ""
     line_num = 1
     while True:
@@ -35,6 +35,6 @@ if file:
         else:
             file_contents = f"{file_contents}{line_num} {file_input}\n"
             line_num += 1
-    with open(os.path.join(*dirs, file), "a") as f:
-        f.write(f"{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n")
+    with open(os.path.join(*dirs, file_name), "a") as f:
+        f.write(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
         f.write(f"{file_contents}")
